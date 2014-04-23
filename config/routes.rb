12b282e334ecc7d1a7707d774a149d2e0839fef9
Joaquín Muñoz M <joaquin.munoz@alumnos.inf.utfsm.cl>
2014-04-23 17:07:31 -0300

    agregamos acción edit para los posts y cambiamos el índice

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blog::Application.routes.draw do
   resources :posts
-  root to: "welcome#index"
+  #root to: "welcome#index"
+  root to: "posts#index"
   get "welcome/index"
 
   #root 'welcome#index'
